9109bc8bc39fc575f8e8ee87614b40a62f450c21
kaba1102 <39997093+kaba1102@users.noreply.github.com>
2018-06-10 20:44:38 +0300

    Add files via upload

---
+++ labs/Lab 3/Lab3_6(2).js
@@ -0,0 +1,15 @@
+var sum = 0;
+var edge = 2;
+listA:
+for(var i = 2; i < 101; i++){
+    for (var j = 2; j < edge; j++){
+        if(i % j == 0){
+          continue listA;
+      }
+    }
+    if(edge !== 10){
+            edge++;
+        }
+    sum += i;
+}
+alert(sum);
